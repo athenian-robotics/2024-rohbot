20ef1a24683f87d09f65b5e9c706d51af564a183
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2024-01-12 16:51:36 -0800

    Added shooter encoders and control systems, implemented basic state machine structure.

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,38 +1,76 @@
 package frc.robot.subsystems;
 
 
-import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkRelativeEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.lib.SimpleVelocitySystem;
+import org.ejml.dense.row.misc.ImplCommonOps_DDMA;
 
 public class  Shooter extends SubsystemBase {
     private static final int LEFT_ID = 0; // TODO: fill in
     private static final int RIGHT_ID = 0; // TODO: fill
+    private static final int kS = 0;
+    private static final int kV = 0;
+    private static final int kA = 0;
+    private static final int MAX_ERROR = 0;
+    private static final double MAX_CONTROL_EFFORT = 0.0;
+    private static final int MODEL_DEVIATION = 0;
+    private static final int ENCODER_DEVIATION = 0;
+    private static final int LOOPTIME = 0;
 
     private final CANSparkMax shooterL = new CANSparkMax(LEFT_ID, CANSparkLowLevel.MotorType.kBrushless);
     private final CANSparkMax shooterR = new CANSparkMax(RIGHT_ID, CANSparkLowLevel.MotorType.kBrushless);
+    private final RelativeEncoder shooterLEncoder;
+    private final RelativeEncoder shooterREncoder;
     private State state = State.IDLE;
+    private final SimpleVelocitySystem sysL;
+    private final SimpleVelocitySystem sysR;
 
     public Shooter() {
         // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
         //       in the constructor or in the robot coordination class, such as RobotContainer.
         //       Also, you can call addChild(name, sendableChild) to associate sendables with the subsystem
         //       such as SpeedControllers, Encoders, DigitalInputs, etc.
-        
+        shooterLEncoder = shooterL.getEncoder(SparkRelativeEncoder.Type.kHallSensor, 42);
+        shooterREncoder = shooterR.getEncoder(SparkRelativeEncoder.Type.kHallSensor, 42);
+
+        sysL = new SimpleVelocitySystem(kS, kV, kA, MAX_ERROR, MAX_CONTROL_EFFORT, MODEL_DEVIATION, ENCODER_DEVIATION, LOOPTIME);
+        sysR = new SimpleVelocitySystem(kS, kV, kA, MAX_ERROR, MAX_CONTROL_EFFORT, MODEL_DEVIATION, ENCODER_DEVIATION, LOOPTIME);
     }
 
     private enum State {
         IDLE, // default state
-        READY, // at setpoint and withing tolerance
+        READY, // at setpoint and within tolerance
         APPROACHING, // approaching setpoint
         TESTING // for collecting shooter data table values
     }
 
+    private boolean atSetpoint() {
+        return Math.abs(sysL.getError()) < MAX_ERROR && Math.abs(sysR.getError()) < MAX_ERROR;
+    }
+
     @Override
     public void periodic() {
-
+        sysL.update(shooterLEncoder.getVelocity()); // Returns RPM
+        sysR.update(shooterREncoder.getVelocity());
+        switch (state) {
+            case IDLE:
+                sysL.set(0.0);
+                sysR.set(0.0);
+                break;
+            case TESTING:
+                // log values
+                break;
+            case APPROACHING:
+                // send limelight data to data table, send result to system
+                break;
+        }
+        if (atSetpoint()) {
+            state = State.READY;
+        }
     }
 }
 
