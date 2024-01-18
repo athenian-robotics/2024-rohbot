e04be7e016ef515a2696e2b68c5ca4f1a6b3f3da
Ken <kenhuangsy@gmail.com>
2024-01-18 15:09:12 -0800

    Set up swerve subsystem.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,16 +4,36 @@
 
 package frc.robot;
 
+import java.io.File;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.Swerve;
 
 public class RobotContainer {
-  //  InterpolatingMatrixTreeMap
+  private final Swerve drivebase = new Swerve(new File(Filesystem.getDeployDirectory(),
+      "swerve"));
+
+  XboxController driverXbox = new XboxController(0); // TODO: Replace with actual port #
+  private final double LEFT_X_DEADBAND = 0.01;
+  private final double LEFT_Y_DEADBAND = 0.01;
+
   public RobotContainer() {
+    Command driveFieldOrientedDirectAngle = drivebase.driveCommand(
+        () -> MathUtil.applyDeadband(driverXbox.getLeftY(), LEFT_Y_DEADBAND),
+        () -> MathUtil.applyDeadband(driverXbox.getLeftX(), LEFT_X_DEADBAND),
+        () -> driverXbox.getRightX(),
+        () -> driverXbox.getRightY());
+    drivebase.setDefaultCommand(driveFieldOrientedDirectAngle);
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -1,19 +1,59 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+
 import java.io.File;
-import java.io.IOException;
+import java.util.function.DoubleSupplier;
+
 import swervelib.SwerveDrive;
+import swervelib.math.SwerveMath;
 import swervelib.parser.SwerveParser;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
 
-public class Swerve {
-  private static final double MAX_SPEED = 0.0; // TODO: real value
-  private File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(), "swerve");
-  private SwerveDrive swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(MAX_SPEED);
+public class Swerve extends SubsystemBase {
+  private final double maximumSpeed = Units.feetToMeters(0); // TODO: replace dummy value with the real value of the max
+  // speed in m/s
+  private SwerveDrive swerveDrive;
 
-  public Swerve() throws IOException {
-
+  public Swerve(File swerveJsonDirectory) {
+    // Angle conversion factor = 360 / (GEAR RATIO * ENCODER RESOLUTION)
+    double angleConversionFactor = SwerveMath.calculateDegreesPerSteeringRotation(0, 0); // TODO: Replace dummy values
+                                                                                         // with real values
+    // Motor conversion factor is (PI * WHEEL DIAMETER IN METERS) / (GEAR RATIO *
+    // ENCODER RESOLUTION).
+    double driveConversionFactor = SwerveMath.calculateMetersPerRotation(Units.inchesToMeters(0), 0, 0);
+    SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+    try {
+      swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(maximumSpeed, angleConversionFactor,
+          driveConversionFactor);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    swerveDrive.setHeadingCorrection(false);
   }
 
+  public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier headingX,
+      DoubleSupplier headingY) {
+    // swerveDrive.setHeadingCorrection(true); // Normally you would want heading
+    // correction for this kind of control.
+    return run(() -> {
+      double xInput = Math.pow(translationX.getAsDouble(), 3); // Smooth controll out
+      double yInput = Math.pow(translationY.getAsDouble(), 3); // Smooth controll out
+      // Make the robot move
+      driveFieldOriented(swerveDrive.swerveController.getTargetSpeeds(xInput, yInput,
+          headingX.getAsDouble(),
+          headingY.getAsDouble(),
+          swerveDrive.getYaw().getRadians(),
+          swerveDrive.getMaximumVelocity()));
+    });
+  }
 
+  public void driveFieldOriented(ChassisSpeeds velocity) {
+    swerveDrive.driveFieldOriented(velocity);
+  }
 }
