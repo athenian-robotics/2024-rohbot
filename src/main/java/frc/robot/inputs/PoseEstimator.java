91e73a0274f050d7c32822a0b3b69baa36eb4d51
Ken <kenhuangsy@gmail.com>
2024-01-19 17:04:46 -0800

    pose estimator class updated with getpose

--- src/main/java/frc/robot/inputs/PoseEstimator.java
+++ src/main/java/frc/robot/inputs/PoseEstimator.java
@@ -1,3 +1,20 @@
 package frc.robot.inputs;
 
-public class PoseEstimator {}
+import org.photonvision.PhotonCamera;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import swervelib.SwerveDrive;
+
+public class PoseEstimator {
+    private final PhotonCamera photonCamera;
+    private final SwerveDrive swerveDrive;
+
+    public PoseEstimator(PhotonCamera photonCamera, SwerveDrive swerveDrive) {
+        this.photonCamera = photonCamera;
+        this.swerveDrive = swerveDrive;
+    }
+
+    public Pose2d getPose() {
+        return swerveDrive.getPose();
+    }
+}
