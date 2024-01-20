8bf2b7a6e3e9b8041946a2455a783e71cc654ac1
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2024-01-19 17:00:02 -0800

    Created NoteDetector. Code will error until PoseEstimator is created.

---
+++ src/main/java/frc/robot/inputs/NoteDetector.java
@@ -0,0 +1,56 @@
+package frc.robot.inputs;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.units.Distance;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.Units.*;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class NoteDetector {
+    private static final Measure<Distance> CAMERA_HEIGHT = Units.Feet.of(1); // TODO: Set to actual value
+    private static final Rotation2d CAMERA_PITCH = new Rotation2d(0.0); // TODO: Set to actual value
+    private final PhotonCamera camera;
+    private final PoseEstimator poseEstimator;
+
+    public NoteDetector (PhotonCamera camera, PoseEstimator poseEstimator) {
+        this.camera = camera;
+        this.poseEstimator = poseEstimator;
+    }
+
+    public Optional<List<Translation2d>> get () {
+        PhotonPipelineResult result = camera.getLatestResult();
+
+        if (result.hasTargets()) {
+            ArrayList<Translation2d> positions = new ArrayList<>();
+
+            List<PhotonTrackedTarget> targets = result.getTargets();
+
+            for (PhotonTrackedTarget target: targets) {
+                Measure<Distance> distance = Units.Meters.of(PhotonUtils.calculateDistanceToTargetMeters(
+                        CAMERA_HEIGHT.in(Units.Meters),
+                        0,
+                        CAMERA_PITCH.getRadians(),
+                        Units.Degrees.of(target.getPitch()).in(Units.Radians)));
+
+                Translation2d translation = PhotonUtils.estimateCameraToTargetTranslation(distance.in(Units.Meters),
+                        Rotation2d.fromDegrees(-target.getYaw()));
+
+                positions.add(poseEstimator.getPose().getTranslation().plus(translation));
+            }
+
+            return Optional.of(positions);
+        }
+
+        return Optional.empty();
+    }
+}
