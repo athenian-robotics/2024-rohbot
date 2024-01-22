4198969200c0b26ca66241c611c0c4ba7b033821
Ken <kenhuangsy@gmail.com>
2024-01-22 15:22:40 -0800

    Updated pose estimator to return the position with swerve drive pose estimator -- did with david.

---
+++ src/main/java/frc/robot/SwerveModule.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+
+public class SwerveModule {
+  private static final double kWheelRadius = 0.0508; // TODO: Replace with real wheel radius
+  private static final int kEncoderResolution = 4096; // TODO: Replace with real encoder resolution
+
+  private final Encoder m_driveEncoder;
+  private final Encoder m_turningEncoder;
+
+  /**
+   * Constructs a SwerveModule with a drive motor, turning motor, drive encoder and turning encoder.
+   *
+   * @param driveEncoderChannelA DIO input for the drive encoder channel A
+   * @param driveEncoderChannelB DIO input for the drive encoder channel B
+   * @param turningEncoderChannelA DIO input for the turning encoder channel A
+   * @param turningEncoderChannelB DIO input for the turning encoder channel B
+   */
+  public SwerveModule(
+      int driveEncoderChannelA,
+      int driveEncoderChannelB,
+      int turningEncoderChannelA,
+      int turningEncoderChannelB) {
+
+    m_driveEncoder = new Encoder(driveEncoderChannelA, driveEncoderChannelB);
+    m_turningEncoder = new Encoder(turningEncoderChannelA, turningEncoderChannelB);
+
+    m_driveEncoder.setDistancePerPulse(2 * Math.PI * kWheelRadius / kEncoderResolution);
+
+    m_turningEncoder.setDistancePerPulse(2 * Math.PI / kEncoderResolution);
+  }
+
+
+  public SwerveModulePosition getPosition() {
+    return new SwerveModulePosition(
+        m_driveEncoder.getDistance(), new Rotation2d(m_turningEncoder.getDistance()));
+  }
+}
--- src/main/java/frc/robot/inputs/PoseEstimator.java
+++ src/main/java/frc/robot/inputs/PoseEstimator.java
@@ -22,34 +22,50 @@ import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.SwerveModule;
 import swervelib.SwerveDrive;
 
 import java.util.Optional;
 
-public class PoseEstimator {
+public class PoseEstimator implements Subsystem {
     private final AprilTagFieldLayout aprilTagFieldLayout;
     private final Transform3d robotToCam;
     private final PhotonPoseEstimator photonPoseEstimator;
     private final Pigeon2 gyro;
     private final SwerveDriveKinematics m_kinematics;
+
+    private final SwerveModule frontLeftModule;
+    private final SwerveModule backLeftModule;
+    private final SwerveModule frontRightModule;
+    private final SwerveModule backRightModule;
+
     private final SwerveDrivePoseEstimator swerveDrivePoseEstimator;
 
     public PoseEstimator(PhotonCamera photonCamera, SwerveDrive swerveDrive) throws IOException {
+        frontLeftModule = new SwerveModule(0, 0, 0, 0); // TODO: Replace with actual ports
+        backLeftModule = new SwerveModule(0, 0, 0, 0);
+        frontRightModule = new SwerveModule(0, 0, 0, 0);
+        backRightModule = new SwerveModule(0, 0, 0, 0);
         m_kinematics = new SwerveDriveKinematics(
                 new Translation2d(0.3048, 0.3048),
                 new Translation2d(0.3048, -0.3048),
                 new Translation2d(-0.3048, 0.3048),
                 new Translation2d(-0.3048, -0.3048));
         gyro = new Pigeon2(4, "*"); // TODO: replace with device id
+
         aprilTagFieldLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2024Crescendo.m_resourceFile);
         robotToCam = new Transform3d(
                 new Translation3d(Units.inchesToMeters(14.5), Units.inchesToMeters(4.5), Units.inchesToMeters(18)),
-                new Rotation3d(0, 0, 0)); // TODO: Replace with
-        // real cam pos
+                new Rotation3d(0, 0, 0)); // TODO: Replace with real cam pos
         photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE,
                 photonCamera, robotToCam);
-        swerveDrivePoseEstimator = new SwerveDrivePoseEstimator(m_kinematics, gyro.getRotation2d(), null, // TODO: add module positions
+
+        swerveDrivePoseEstimator = new SwerveDrivePoseEstimator(m_kinematics, gyro.getRotation2d(), getCurrentSwerveModulePositions(),
                 new Pose2d(0, 0, new Rotation2d()), VecBuilder.fill(0.05, 0.05, Units.degreesToRadians(5)),
                 VecBuilder.fill(0.5, 0.5, Units.degreesToRadians(30)));
     }
@@ -58,9 +74,27 @@ public class PoseEstimator {
         Optional<EstimatedRobotPose> estimatedRobotPose = photonPoseEstimator.update();
         if (estimatedRobotPose.isPresent()) {
             Pose3d pose3d = estimatedRobotPose.get().estimatedPose;
-            return new Translation2d(pose3d.getX(), pose3d.getY()); // TODO:  then feed PhotonPoseEstimator into SwerveDrivePoseEstimator via addVisionMeasurement() 
+            Translation2d translation = new Translation2d(pose3d.getX(), pose3d.getY());
+            swerveDrivePoseEstimator.addVisionMeasurement(new Pose2d(translation, gyro.getRotation2d()), Timer.getFPGATimestamp());
+            Pose2d swervePose2d = swerveDrivePoseEstimator.getEstimatedPosition();
+            return new Translation2d(swervePose2d.getX(), swervePose2d.getY());
         } else {
             return null;
         }
+    }
+
+    public SwerveModulePosition[] getCurrentSwerveModulePositions()
+    {
+        return new SwerveModulePosition[]{
+            frontLeftModule.getPosition(),
+            backLeftModule.getPosition(),
+            frontRightModule.getPosition(),
+            backRightModule.getPosition()
+        };
+    }
+
+    @Override
+    public void periodic() {
+        swerveDrivePoseEstimator.update(gyro.getRotation2d(), getCurrentSwerveModulePositions());
     }
 }
