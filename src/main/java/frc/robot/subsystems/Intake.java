b7971d44420ec69d0ec8dfbde8014b584ba17554
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2024-01-25 16:33:28 -0800

    Implemented state machine for Intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -3,15 +3,53 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.Rev2mDistanceSensor;
+import edu.wpi.first.units.Distance;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
   private static final int motorId = 0; // TODO: Fill out value
+  private static final Measure<Distance> NOTE_FOUND_THRESHOLD = Units.Inches.of(0); // TODO: Tune
+  private static final Measure<Distance> NOTE_PASSED_THRESHOLD = Units.Inches.of(0); // TODO: Tune
   private final CANSparkMax motor;
   private final Rev2mDistanceSensor sensor;
+  private enum State {
+    NO_NOTE,
+    NOTE_FOUND,
+    NOTE_PASSED
+  }
+  private State state;
 
   public Intake() {
     motor = new CANSparkMax(motorId, CANSparkLowLevel.MotorType.kBrushless);
     sensor = new Rev2mDistanceSensor(Rev2mDistanceSensor.Port.kOnboard);
+    state = State.NO_NOTE;
+  }
+
+  public Command shotFired() {
+    return new InstantCommand(() -> state = State.NO_NOTE, this);
+  }
+
+  @Override
+  public void periodic() {
+    switch (state) {
+      case NO_NOTE:
+        motor.set(1); // TODO: Tune speed
+        if (sensor.getRange(Rev2mDistanceSensor.Unit.kInches) < NOTE_FOUND_THRESHOLD.in(Units.Inches)) {
+          state = State.NOTE_FOUND;
+        }
+        break;
+      case NOTE_FOUND:
+        motor.set(1); // TODO: Tune
+        if (sensor.getRange(Rev2mDistanceSensor.Unit.kInches) > NOTE_PASSED_THRESHOLD.in(Units.Inches)) {
+          state = State.NOTE_PASSED;
+        }
+        break;
+      case NOTE_PASSED:
+        motor.set(0);
+    }
   }
 }
