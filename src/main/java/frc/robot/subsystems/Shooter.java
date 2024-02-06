9032b4d435db8d621fa874fb862e5f3c51fde8aa
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2024-02-05 16:43:30 -0800

    shooter part

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -23,8 +23,10 @@ import monologue.Annotations.Log;
 
 public class Shooter extends SubsystemBase {
   // TODO: fill in values
-  private static final int LEFT_ID = 0;
-  private static final int RIGHT_ID = 0;
+  private static final int LEFT_DRIVE_ID = 0;
+  private static final int RIGHT_DRIVE_ID = 0;
+  private static final int LEAD_TRIGGER_ID = 0;
+  private static final int FOLLOW_TRIGGER_ID = 0;
   private static final double kS = 0;
   private static final double kV = 0;
   private static final double kA = 0;
@@ -34,10 +36,12 @@ public class Shooter extends SubsystemBase {
   private static final double ENCODER_DEVIATION = 0;
   private static final double LOOPTIME = 0.02;
 
-  private final CANSparkMax motorL =
-      new CANSparkMax(LEFT_ID, CANSparkLowLevel.MotorType.kBrushless);
-  private final CANSparkMax motorR =
-      new CANSparkMax(RIGHT_ID, CANSparkLowLevel.MotorType.kBrushless);
+  private final CANSparkMax driveL =
+          new CANSparkMax(LEFT_DRIVE_ID, CANSparkLowLevel.MotorType.kBrushless);
+  private final CANSparkMax driveR =
+          new CANSparkMax(RIGHT_DRIVE_ID, CANSparkLowLevel.MotorType.kBrushless);
+  private final CANSparkMax leadTrigger = new CANSparkMax(LEAD_TRIGGER_ID, CANSparkLowLevel.MotorType.kBrushless);
+  private final CANSparkMax followTrigger = new CANSparkMax(FOLLOW_TRIGGER_ID, CANSparkLowLevel.MotorType.kBrushless);
   private final RelativeEncoder encoderL;
   private final RelativeEncoder encoderR;
   private final SimpleVelocitySystem sysL;
@@ -52,59 +56,61 @@ public class Shooter extends SubsystemBase {
   private Translation2d translationToSpeaker;
 
   public Shooter(ShooterDataTable table) {
-    encoderL = motorL.getEncoder(SparkRelativeEncoder.Type.kHallSensor, 42);
-    encoderR = motorR.getEncoder(SparkRelativeEncoder.Type.kHallSensor, 42);
+    encoderL = driveL.getEncoder(SparkRelativeEncoder.Type.kHallSensor, 42);
+    encoderR = driveR.getEncoder(SparkRelativeEncoder.Type.kHallSensor, 42);
+
+    followTrigger.follow(leadTrigger);
 
     this.table = table;
 
     sysL =
-        new SimpleVelocitySystem(
-            kS,
-            kV,
-            kA,
-            MAX_ERROR,
-            MAX_CONTROL_EFFORT,
-            MODEL_DEVIATION,
-            ENCODER_DEVIATION,
-            LOOPTIME);
+            new SimpleVelocitySystem(
+                    kS,
+                    kV,
+                    kA,
+                    MAX_ERROR,
+                    MAX_CONTROL_EFFORT,
+                    MODEL_DEVIATION,
+                    ENCODER_DEVIATION,
+                    LOOPTIME);
     sysR =
-        new SimpleVelocitySystem(
-            kS,
-            kV,
-            kA,
-            MAX_ERROR,
-            MAX_CONTROL_EFFORT,
-            MODEL_DEVIATION,
-            ENCODER_DEVIATION,
-            LOOPTIME);
+            new SimpleVelocitySystem(
+                    kS,
+                    kV,
+                    kA,
+                    MAX_ERROR,
+                    MAX_CONTROL_EFFORT,
+                    MODEL_DEVIATION,
+                    ENCODER_DEVIATION,
+                    LOOPTIME);
     routineL =
-        new SysIdRoutine(
-            new SysIdRoutine.Config(Volts.per(Seconds).of(1), Volts.of(12), Seconds.of(10)),
-            new SysIdRoutine.Mechanism(
-                (Measure<Voltage> volts) -> motorL.setVoltage(volts.in(Volts)),
-                this::logL,
-                this,
-                "left-flywheel-motor"));
+            new SysIdRoutine(
+                    new SysIdRoutine.Config(Volts.per(Seconds).of(1), Volts.of(12), Seconds.of(10)),
+                    new SysIdRoutine.Mechanism(
+                            (Measure<Voltage> volts) -> driveL.setVoltage(volts.in(Volts)),
+                            this::logL,
+                            this,
+                            "left-flywheel-motor"));
     routineR =
-        new SysIdRoutine(
-            new SysIdRoutine.Config(Volts.per(Seconds).of(1), Volts.of(12), Seconds.of(10)),
-            new SysIdRoutine.Mechanism(
-                (Measure<Voltage> volts) -> motorR.setVoltage(volts.in(Volts)),
-                this::logR,
-                this,
-                "right flywheel motor"));
+            new SysIdRoutine(
+                    new SysIdRoutine.Config(Volts.per(Seconds).of(1), Volts.of(12), Seconds.of(10)),
+                    new SysIdRoutine.Mechanism(
+                            (Measure<Voltage> volts) -> driveR.setVoltage(volts.in(Volts)),
+                            this::logR,
+                            this,
+                            "right flywheel motor"));
   }
 
   private void logR(SysIdRoutineLog log) {
     log.motor("right-flywheel-motor")
-        .voltage(appliedVoltage.mut_replace(motorL.getBusVoltage() * motorL.get(), Volts))
-        .angularVelocity(velocity.mut_replace(encoderL.getVelocity(), Rotations.per(Minute)));
+            .voltage(appliedVoltage.mut_replace(driveL.getBusVoltage() * driveL.get(), Volts))
+            .angularVelocity(velocity.mut_replace(encoderL.getVelocity(), Rotations.per(Minute)));
   }
 
   private void logL(SysIdRoutineLog log) {
     log.motor("left-flywheel-motor")
-        .voltage(appliedVoltage.mut_replace(motorR.getBusVoltage() * motorR.get(), Volts))
-        .angularVelocity(velocity.mut_replace(encoderR.getVelocity(), Rotations.per(Minute)));
+            .voltage(appliedVoltage.mut_replace(driveR.getBusVoltage() * driveR.get(), Volts))
+            .angularVelocity(velocity.mut_replace(encoderR.getVelocity(), Rotations.per(Minute)));
   }
 
   @Log.NT
@@ -123,11 +129,11 @@ public class Shooter extends SubsystemBase {
 
   public Command requestShot(Translation2d translationToSpeaker) {
     return new InstantCommand(
-        () -> {
-          state = State.APPROACHING;
-          this.translationToSpeaker = translationToSpeaker;
-        },
-        this);
+            () -> {
+              state = State.APPROACHING;
+              this.translationToSpeaker = translationToSpeaker;
+            },
+            this);
   }
 
   public Command idle() {
@@ -162,6 +168,10 @@ public class Shooter extends SubsystemBase {
     return routineR.dynamic(direction);
   }
 
+  public Command fire() {
+    return new InstantCommand(() -> state = State.FIRING, this);
+  }
+
   @Override
   public void periodic() {
     sysL.update(getWheelSpeedL()); // Returns RPM
@@ -180,6 +190,9 @@ public class Shooter extends SubsystemBase {
         sysL.set(spec.speedL());
         sysR.set(spec.speedR());
         break;
+      case FIRING:
+        leadTrigger.set(0.5); // TODO: Tune
+        break;
       case SYSID:
         break;
     }
@@ -194,5 +207,6 @@ public class Shooter extends SubsystemBase {
     APPROACHING, // approaching setpoint
     TESTING, // for collecting shooter data table values
     SYSID, // for system identification
+    FIRING,
   }
 }
