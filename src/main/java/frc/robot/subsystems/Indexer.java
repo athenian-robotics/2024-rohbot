9bb51a07da2938af280e3b7b9eaf350a2c8528c4
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2024-02-05 16:44:50 -0800

    indexer part

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -21,7 +21,8 @@ import lombok.Getter;
 
 public class Indexer extends SubsystemBase {
   private static final int INDEXER_MOTOR_ID = 0; // TODO: ADD MOTOR PORTS ACCURATELY
-  private static final int ANGLE_MOTOR_ID = 0; // TODO: port
+  private static final int LEAD_ANGLE_MOTOR_ID = 0; // TODO: port
+  private static final int FOLLOW_ANGLE_MOTOR_ID = 0; // TODO: number
   private static final double kV = 0; // TODO: Sysid
   private static final double kA = 0; // TODO: sysid
   private static final double kS = 0;
@@ -36,7 +37,8 @@ public class Indexer extends SubsystemBase {
   private static final Measure<Distance> SHOT_FIRED_THRESHOLD = Units.Inches.of(0); // TODO: Tune
   private static final Measure<Time> ROBOT_TIME_STEP = Units.Milli(Units.Seconds).of(50);
   private final CANSparkMax indexMotor;
-  private final CANSparkMax angleMotor;
+  private final CANSparkMax leadAngleMotor;
+  private final CANSparkMax followAngleMotor;
   private final Rev2mDistanceSensor sensor;
   private final LinearSystemLoop<N2, N1, N1> loop;
   private final ShooterDataTable table;
@@ -47,40 +49,42 @@ public class Indexer extends SubsystemBase {
 
   public Indexer(ShooterDataTable table) {
     indexMotor = new CANSparkMax(INDEXER_MOTOR_ID, CANSparkLowLevel.MotorType.kBrushless);
-    angleMotor = new CANSparkMax(ANGLE_MOTOR_ID, CANSparkLowLevel.MotorType.kBrushless);
+    leadAngleMotor = new CANSparkMax(LEAD_ANGLE_MOTOR_ID, CANSparkLowLevel.MotorType.kBrushless);
+    followAngleMotor = new CANSparkMax(FOLLOW_ANGLE_MOTOR_ID, CANSparkLowLevel.MotorType.kBrushless);
+    followAngleMotor.follow(leadAngleMotor);
     this.table = table;
     sensor =
-        new Rev2mDistanceSensor(Rev2mDistanceSensor.Port.kOnboard); // TODO: Figure out right value
+            new Rev2mDistanceSensor(Rev2mDistanceSensor.Port.kOnboard); // TODO: Figure out right value
 
     LinearSystem<N2, N1, N1> sys = LinearSystemId.identifyPositionSystem(kV, kA);
 
     KalmanFilter<N2, N1, N1> filter =
-        new KalmanFilter<>(
-            Nat.N2(),
-            Nat.N1(),
-            sys,
-            VecBuilder.fill(
-                ANGLE_STANDARD_DEVIATION.in(Units.Radians),
-                ANGLE_SPEED_STANDARD_DEVIATION.in(Units.RadiansPerSecond)),
-            VecBuilder.fill(TICKS_TO_ANGLE.in(Units.Radians)),
-            ROBOT_TIME_STEP.in(Units.Seconds));
+            new KalmanFilter<>(
+                    Nat.N2(),
+                    Nat.N1(),
+                    sys,
+                    VecBuilder.fill(
+                            ANGLE_STANDARD_DEVIATION.in(Units.Radians),
+                            ANGLE_SPEED_STANDARD_DEVIATION.in(Units.RadiansPerSecond)),
+                    VecBuilder.fill(TICKS_TO_ANGLE.in(Units.Radians)),
+                    ROBOT_TIME_STEP.in(Units.Seconds));
 
     LinearQuadraticRegulator<N2, N1, N1> controller =
-        new LinearQuadraticRegulator<>(
-            sys,
-            VecBuilder.fill(
-                ANGLE_ERROR_TOLERANCE.in(Units.Radians),
-                ANGLE_SPEED_ERROR_TOLERANCE.in(Units.RadiansPerSecond)),
-            VecBuilder.fill(MAX_ANGLE_MOTOR_VOLTAGE.in(Units.Volts)),
-            ROBOT_TIME_STEP.in(Units.Seconds));
+            new LinearQuadraticRegulator<>(
+                    sys,
+                    VecBuilder.fill(
+                            ANGLE_ERROR_TOLERANCE.in(Units.Radians),
+                            ANGLE_SPEED_ERROR_TOLERANCE.in(Units.RadiansPerSecond)),
+                    VecBuilder.fill(MAX_ANGLE_MOTOR_VOLTAGE.in(Units.Volts)),
+                    ROBOT_TIME_STEP.in(Units.Seconds));
 
     loop =
-        new LinearSystemLoop<>(
-            sys,
-            controller,
-            filter,
-            MAX_ANGLE_MOTOR_VOLTAGE.in(Units.Volts),
-            ROBOT_TIME_STEP.in(Units.Seconds));
+            new LinearSystemLoop<>(
+                    sys,
+                    controller,
+                    filter,
+                    MAX_ANGLE_MOTOR_VOLTAGE.in(Units.Volts),
+                    ROBOT_TIME_STEP.in(Units.Seconds));
   }
 
   public boolean isLoading() {
@@ -97,8 +101,8 @@ public class Indexer extends SubsystemBase {
 
   public boolean isInactive() {
     return this.getState() == State.EMPTY
-        && !(this.getState() == State.LOADING)
-        && !(this.getState() == State.LOADED);
+            && !(this.getState() == State.LOADING)
+            && !(this.getState() == State.LOADED);
   }
 
   public Command startLoading() {
@@ -122,7 +126,7 @@ public class Indexer extends SubsystemBase {
       case LOADING -> {
         indexMotor.set(1); // TODO: Tune
         if (sensor.getRange(Rev2mDistanceSensor.Unit.kInches)
-            < NOTE_LOADED_THRESHOLD.in(Units.Inches)) {
+                < NOTE_LOADED_THRESHOLD.in(Units.Inches)) {
           state = State.LOADED;
         }
       }
@@ -130,23 +134,23 @@ public class Indexer extends SubsystemBase {
         indexMotor.set(0);
         loop.setNextR(table.get(translationToSpeaker).angle());
         if (loop.getError(1) < ANGLE_ERROR_TOLERANCE.in(Units.Radians)
-            && loop.getError(2) < ANGLE_SPEED_ERROR_TOLERANCE.in(Units.RadiansPerSecond)) {
+                && loop.getError(2) < ANGLE_SPEED_ERROR_TOLERANCE.in(Units.RadiansPerSecond)) {
           state = State.READY;
         }
       }
       case FIRING -> {
         indexMotor.set(1); // TODO: Tune
         if (sensor.getRange(Rev2mDistanceSensor.Unit.kInches)
-            < SHOT_FIRED_THRESHOLD.in(Units.Inches)) {
+                < SHOT_FIRED_THRESHOLD.in(Units.Inches)) {
           state = State.EMPTY;
         }
       }
     }
     loop.correct(
-        VecBuilder.fill(angleMotor.getEncoder().getPosition() * TICKS_TO_ANGLE.in(Units.Radians)));
+            VecBuilder.fill(leadAngleMotor.getEncoder().getPosition() * TICKS_TO_ANGLE.in(Units.Radians)));
     loop.predict(ROBOT_TIME_STEP.in(Units.Seconds));
-    angleMotor.set(
-        loop.getU(0) + kS * Math.signum(loop.getNextR(1) + kG * Math.cos(loop.getNextR(0))));
+    leadAngleMotor.set(
+            loop.getU(0) + kS * Math.signum(loop.getNextR(1) + kG * Math.cos(loop.getNextR(0))));
   }
 
   private enum State {
