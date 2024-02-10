2980371d4b7c2b0f4f2ccfc06ac344074cd96866
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2024-02-09 17:30:35 -0800

    pidf changes

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -10,19 +10,15 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import java.io.File;
 import java.util.function.DoubleSupplier;
-
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import swervelib.SwerveDrive;
 import swervelib.SwerveDriveTest;
 import swervelib.math.SwerveMath;
 import swervelib.parser.SwerveParser;
 import swervelib.telemetry.SwerveDriveTelemetry;
 import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
-
-import static edu.wpi.first.units.Units.Seconds;
-import static edu.wpi.first.units.Units.Volts;
 
 public class Swerve extends SubsystemBase {
   private final double maximumSpeed = Units.feetToMeters(20);
@@ -86,20 +82,20 @@ public class Swerve extends SubsystemBase {
   }
 
   public Command sysIdDriveCommand() {
-      return SwerveDriveTest.generateSysIdCommand(
-              SwerveDriveTest.setDriveSysIdRoutine(
-                      new SysIdRoutine.Config(),
-                      this, swerveDrive, 12),
-              3.0, 5.0, 3.0);
+    return SwerveDriveTest.generateSysIdCommand(
+        SwerveDriveTest.setDriveSysIdRoutine(new SysIdRoutine.Config(), this, swerveDrive, 12),
+        3.0,
+        5.0,
+        3.0);
   }
 
-    public Command sysIdAngleMotorCommand() {
-        return SwerveDriveTest.generateSysIdCommand(
-                SwerveDriveTest.setAngleSysIdRoutine(
-                        new SysIdRoutine.Config(),
-                        this, swerveDrive),
-                3.0, 5.0, 3.0);
-    }
+  public Command sysIdAngleMotorCommand() {
+    return SwerveDriveTest.generateSysIdCommand(
+        SwerveDriveTest.setAngleSysIdRoutine(new SysIdRoutine.Config(), this, swerveDrive),
+        3.0,
+        5.0,
+        3.0);
+  }
 
   public Command resetHeading() {
     return new InstantCommand(() -> swerveDrive.zeroGyro(), this);
